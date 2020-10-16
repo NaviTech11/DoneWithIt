ae5ecf28b1df1c43808f59c6696a69738aac4e50
Ivan Hernandez <ivan.hdz2@outlook.com>
2020-10-16 15:41:17 -0500

    Add Welcome Screen

--- App.js
+++ App.js
@@ -1,43 +1,13 @@
 import React from 'react';
-import { View} from 'react-native';
+import { View } from 'react-native';
+import WelcomeScreen from './app/screens/WelcomeScreen';
 
 
 export default function App() {
   
   return (
-   
-        <View style={{ 
-          backgroundColor: "#fff",
-          flex: 1,
-          flexDirection: "row",  //axis (horizontal)
-          justifyContent: "center", //alig items accross the main axis
-          alignItems: "center", //align axis accross secondary axis
-          }}>
-         
-          <View style={{
-            backgroundColor: "dodgerblue",
-            // flexBasis: 100, // map to width or height
-            // flexGrow: 1, //fill avalable space
-            flex: -1, //negative numbers in flex. same as flexShrink
-            width: 400,
-            height: 100,
-            
-          }}/>
-
-          <View style={{
-            backgroundColor: "gold",
-            width: 100,
-            height: 100,
-          }}/>
-
-          <View style={{
-            backgroundColor: "tomato",
-            width: 100,
-            height: 100,
-          }}/>
-
-          
-        </View>
-    
+     
+    <WelcomeScreen />
+             
   );
 }
