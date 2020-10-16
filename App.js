8ebfcd7068b24094b53770861092a24d50411964
Ivan Hernandez <ivan.hdz2@outlook.com>
2020-10-16 13:33:42 -0500

    Layout: Dimensions & Detecting Orientation Changes

--- App.js
+++ App.js
@@ -1,17 +1,18 @@
 import React from 'react';
-import { StyleSheet, Button, Alert, SafeAreaView, StatusBar, Platform } from 'react-native';
+import { StyleSheet, View, SafeAreaView, StatusBar, Platform } from 'react-native';
+import { useDimensions, useDeviceOrientation } from '@react-native-community/hooks';
 
 export default function App() {
-  
+  const { landscape } = useDeviceOrientation();
   return (
     <SafeAreaView style={styles.container}>
-      <Button 
-        title="Click Me" 
-        onPress={() => 
-        Alert.prompt("My title", "My message", text => console.log(text))
-        }
-        color="orange"
-      />
+        <View style={{
+          backgroundColor: 'dodgerblue',
+          width: "100%",
+          height: landscape ? "100%" : "30%",
+          }}>
+
+        </View>
     </SafeAreaView>
   );
 }
