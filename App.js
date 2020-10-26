c7812a4fcc8eda8074efa958fc57567e83e646c8
Ivan Hernandez <ivan.hdz2@outlook.com>
2020-10-25 19:55:47 -0500

    Add styles, Button, Text components  and listingDetailsScreen

--- App.js
+++ App.js
@@ -2,13 +2,13 @@ import React from 'react';
 import { View } from 'react-native';
 import ViewImageScreen from './app/screens/ViewImageScreen';
 import WelcomeScreen from './app/screens/WelcomeScreen';
-
+import ListingDetailsScreen from './app/screens/ListingDetailsScreen'
 
 export default function App() {
   
   return (
      
-    <ViewImageScreen />
+    <ListingDetailsScreen />
              
   );
 }
