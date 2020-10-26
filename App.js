9992b937b59e5fc422dc1d265b0cf13ea160763f
Ivan Hernandez <ivan.hdz2@outlook.com>
2020-10-26 10:14:34 -0500

    Add Lists, Account, Messages, Listings Screens

--- App.js
+++ App.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import { View } from 'react-native';
+
 import ViewImageScreen from './app/screens/ViewImageScreen';
 import WelcomeScreen from './app/screens/WelcomeScreen';
-import ListingDetailsScreen from './app/screens/ListingDetailsScreen'
+import ListingDetailsScreen from './app/screens/ListingDetailsScreen';
+import AccountScreen from './app/screens/AccountScreen';
+import ListingsScreen from './app/screens/ListingsScreen';
 
 export default function App() {
   
   return (
      
-    <ListingDetailsScreen />
+    <ListingsScreen />
              
   );
 }
