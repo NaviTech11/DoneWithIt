fd6915ff09dd266ad5c66fdd9cd5d27dd8b4891d
Ivan Hernandez <ivan.hdz2@outlook.com>
2020-10-29 08:29:58 -0500

    Requesting Permissions

--- App.js
+++ App.js
@@ -1,6 +1,20 @@
-import React from "react";
-import ListingEditScreen from "./app/screens/ListingEditScreen";
+import React, { useEffect } from "react";
+import * as ImagePicker from 'expo-image-picker';
+
+import Screen from "./app/components/Screen"
 
 export default function App() {
-  return <ListingEditScreen />;
+  //Getting users permision
+  //componentDidMount => for Class components
+  //useEffect hook for functions
+  const requestPermission = async () => {
+    const { granted } = await ImagePicker.requestCameraPermissionsAsync()
+    if(!granted) alert('Youneed to enable permission to access the library')
+  }
+
+  useEffect(() => {
+    requestPermission(); 
+  }, [])
+
+  return <Screen></Screen>
 }
